f9ded7fa217d7a89944866d7cdadba723224e1e0
martafrlobo <74150743+martafrlobo@users.noreply.github.com>
2020-12-06 11:17:32 +0000

    Update knn.py

--- knn.py
+++ knn.py
@@ -8,31 +8,32 @@ def distance(X_train,X_test):
     point2=np.array(X_test)
     return np.linalg.norm(point1-point2)
 
+"""defining accuracy"""
 def accuracy(y_test, predictions):
     accuracy = np.sum(y_test == predictions) / len(y_test)
     return accuracy  
 
-
 class KNN():
-
+     """store k=3"""
     def __init__(self,k=3):
         self.k=k
-
+     """defining training samples and training labels"""
     def train(self,X,y):
         self.X_train=X
         self.y_train=y
 
+    """predict new samples, this can have multiple samples"""
     def test(self,X):
         labels=[self.predict(x) for x in X]
         return np.array(labels)
     
 
     def predict(self,x):
-        #compute distances
+        """compute distances"""
         distances=[distance(x,x_train) for x_train in self.X_train]
-        #get neighbors
+        """get neighbors"""
         index=np.argsort(distances)[:self.k]
         nearest_neighbors=[self.y_train[i] for i in index]
-        #most common
+        """most common values"""
         most_common=Counter(nearest_neighbors).most_common(1)
         return most_common[0][0]
